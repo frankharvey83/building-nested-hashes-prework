ea92ac10ab784ab43d2779818a8774f6a3078921
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-01 21:36:23 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
         :status => ""
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => ""
@@ -34,5 +34,5 @@ def third_challenge
    }
   }
 
-  
+
 end
